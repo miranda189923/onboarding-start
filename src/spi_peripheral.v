8d3cd5726a145d56c7555680c83797f65dba7297
miranda189923 <mirandazhao6@gmail.com>
2025-06-30 22:18:35 -0400

    spi_peripheral.v update

--- src/spi_peripheral.v
+++ src/spi_peripheral.v
@@ -13,7 +13,9 @@ module spi_peripheral (
     output  reg [7:0]   pwm_duty_cycle,   
 );
 
-    reg ncs1, ncs2, sdi1, sdi2, sclk1, sclk2;    // Synchronization registers
+    reg ncs1, ncs2; // Synchronization registers
+    reg sdi1, sdi2;
+    reg sclk1, sclk2;    
     wire sclk_rise = sclk2 & ~sclk1;             // Detect rising edge of sclk
     reg [15:0] shift_reg;                        // Received message (1 R/W bit + 7 address bits + 8 data bits)
     reg [4:0]  bit_count;                        // Number of received bits
@@ -54,17 +56,23 @@ module spi_peripheral (
                     shift_reg <= {shift_reg[14:0], sdi2};     // Shift in new bits during active transaction
                     bit_count <= bit_count + 1;               // Increment bit counter
                 end
+
             end else begin
-                if (bit_count == 16 && shift_reg[15]) begin   // Write command
-                    temp_data <= shift_reg[7:0];              // Store data
-                    temp_addr <= shift_reg[14:8];             // Store address
-                    temp_valid <= 1'b1;                       // Set flag for update in next cycle
-                end
+                if (bit_count == 16) begin
+                    if (shift_reg[15]) begin                  // Write command
+                        temp_data <= shift_reg[7:0];          // Store data
+                        temp_addr <= shift_reg[14:8];         // Store address
+                        temp_valid <= 1'b1;                   // Set flag for update in next cycle
+                    end
                     bit_count <= 5'd0;
                     shift_reg <= 16'd0;
+                end else begin
+                    bit_count <= 5'd0; 
+                    shift_reg <= 16'd0;
+                end
             end
 
-            if (temp_valid) begin                             // Update output register based on stored address
+            if (temp_valid) begin                              // Update output register based on stored address
                 case (temp_addr)
                     7'h00: en_reg_out_7_0 <= temp_data;
                     7'h01: en_reg_out_15_8 <= temp_data;
